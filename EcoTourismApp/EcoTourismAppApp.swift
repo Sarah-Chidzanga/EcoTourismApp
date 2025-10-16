2e0ad70f8d4aa58dfa3cff13826a52891d815853
Sarah Chidzanga <chidzangasarah@gmail.com>
2025-10-16 11:34:27 +0200

    Initial Commit

---
+++ EcoTourismApp/EcoTourismAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EcoTourismAppApp.swift
+//  EcoTourismApp
+//
+//  Created by Sarah  on 16/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct EcoTourismAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
